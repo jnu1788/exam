c83c4504f2b435e911f2145e834214408812d3a3
User <User@User-PC>
2020-08-21 10:42:22 +0900

    20200821 exam

---
+++ ex00/src/Test01/Calculator.java
@@ -0,0 +1,13 @@
+package Test01;
+
+public class Calculator {
+	
+	public int evaluate(String expression) {
+		int sum =0;
+		for(String summand : expression.split("\\+")) {
+			sum+= Integer.valueOf(summand);
+		}
+		return sum;
+		}
+		
+	}
---
+++ ex00/src/Test01/CalculatorTest.java
@@ -0,0 +1,16 @@
+package Test01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void evaluteExpression() {
+		Calculator cal = new Calculator();
+		int sum = cal.evaluate("1+2+3");
+		assertEquals(6, sum);
+	}
+
+}
